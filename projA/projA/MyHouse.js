a9b3d189c106e01e137ae37b97013f0215b2cdbb
Diogo Alex <diogoalexandre01@hotmail.com>
2019-04-07 01:33:57 +0100

    Added a flag to the house

--- projA/projA/MyHouse.js
+++ projA/projA/MyHouse.js
@@ -11,31 +11,32 @@ class MyHouse extends CGFobject {
         this.scene.cubeQuad = new MyUnitCubeQuad(this.scene);
         this.scene.pyramid = new MyPyramid(this.scene, 4, 1);
         this.scene.prism = new MyPrism(this.scene, 10);
+        this.scene.cylinder = new MyCylinder(this.scene, 10);
     }
 
     display() {
 
-        // Draw Cube Quad
+        // first stair tread
         this.scene.pushMatrix();
         this.scene.scale(6, 0.125, 4);
         this.scene.cubeQuad.display();
         this.scene.popMatrix();
 
-        // Draw Cube Quad
+        // second stair tread
         this.scene.pushMatrix();
         this.scene.translate(0, 0.125, 0);
         this.scene.scale(5.75, 0.125, 3.75);
         this.scene.cubeQuad.display();
         this.scene.popMatrix();
 
-        // Draw Cube Quad
+        // third stair tread
         this.scene.pushMatrix();
         this.scene.translate(0, 0.25, 0);
         this.scene.scale(5.5, 0.125, 3.5);
         this.scene.cubeQuad.display();
         this.scene.popMatrix();
 
-        // Draw Prism
+        // left columns
         for (var i = 0; i < 5.2; i+= 0.3713) {
             this.scene.pushMatrix();
             this.scene.translate(-2.60 + i, 0.25, 1.6);
@@ -44,7 +45,7 @@ class MyHouse extends CGFobject {
             this.scene.popMatrix();
         }
 
-        // Draw Prism
+        // right columns
         for (var i = 0; i < 5.2; i+= 0.3713) {
             this.scene.pushMatrix();
             this.scene.translate(-2.60 + i, 0.25, -1.6);
@@ -53,7 +54,7 @@ class MyHouse extends CGFobject {
             this.scene.popMatrix();
         }
 
-        // Draw Prism
+        // front columns
         for (var i = 0; i < 2.8287; i+= 0.3713) {
             this.scene.pushMatrix();
             this.scene.translate(-2.60, 0.25, 1.295 - i);
@@ -62,7 +63,7 @@ class MyHouse extends CGFobject {
             this.scene.popMatrix();
         }
 
-        // Draw Prism
+        // back columns
         for (var i = 0; i < 2.8287; i+= 0.3713) {
             this.scene.pushMatrix();
             this.scene.translate(2.60, 0.25, 1.295 - i);
@@ -71,14 +72,14 @@ class MyHouse extends CGFobject {
             this.scene.popMatrix();
         }
 
-        // Draw Cube Quad
+        // roof fascia
         this.scene.pushMatrix();
         this.scene.translate(0, 1.25, 0);
         this.scene.scale(5.5, 0.125, 3.5);
         this.scene.cubeQuad.display();
         this.scene.popMatrix();
 
-        // Draw Pyramid
+        // roof
         this.scene.pushMatrix();
         this.scene.translate(0, 1.3135, 0);
         this.scene.scale(3.8903, 0.75, 2.475);
@@ -86,10 +87,24 @@ class MyHouse extends CGFobject {
         this.scene.pyramid.display();
         this.scene.popMatrix();
 
-        // Draw Cube Quad
+        // inner building
         this.scene.pushMatrix();
         this.scene.translate(0, 0.75, 0);
         this.scene.scale(4.5, 1, 2.75);
+        this.scene.cubeQuad.display();
+        this.scene.popMatrix();
+
+        // flag pole
+        this.scene.pushMatrix();
+        this.scene.translate(0, 2, 0);
+        this.scene.scale(0.01, 0.75, 0.01);
+        this.scene.cylinder.display();
+        this.scene.popMatrix();
+
+        // flag
+        this.scene.pushMatrix();
+        this.scene.translate(0, 2.6, -0.225);
+        this.scene.scale(0.01, 0.3, 0.45);
         this.scene.cubeQuad.display();
         this.scene.popMatrix();
     }
